202854441d0069e0418d26872778a4a765b5dbb4
Richard S <voidet@nightgen.com>
2015-03-23 07:33:28 +1000

    Initial Commit

---
+++ InfiniteNestedData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfiniteNestedData
+//
+//  Created by Richard S on 23/03/2015.
+//  Copyright (c) 2015 Richard S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
